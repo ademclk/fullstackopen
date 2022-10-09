94170253ee467f12cd4ec756b5e57a131d3b3595
ademclk <ademonurcelik@icloud.com>
2022-10-09 21:20:22 +0300

    Phonebook step1.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,50 @@
+import {useState} from "react";
+
+const App = () => {
+
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+
+    const [newName, setNewName] = useState('')
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const personObject = {
+            name: newName,
+        }
+        setPersons(persons.concat(personObject));
+        setNewName('');
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+
+    }
+
+    return (
+        <div className="App">
+            <h2>Phonebook</h2>
+            <form onSubmit={addNote}>
+                <div>
+                    name:
+                    <input
+                        value={newName}
+                        onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    <button type='submit'>Add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(p => {
+                    return <li key={p.name}>{p.name}</li>
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
