2855cfe423557dc3c89266ddb9c42ff1f2f61ee2
ademclk <ademonurcelik@icloud.com>
2022-10-10 11:20:16 +0300

    Phonebook step6.

--- part2/phonebook/src/components/PersonList.js
+++ part2/phonebook/src/components/PersonList.js
@@ -3,7 +3,7 @@ import React from "react";
 const PersonList = ({persons}) => {
     return (
         persons.map(p => {
-            return <li key={p.name}>{p.name} {p.phone}</li>
+            return <li key={p.id}>{p.name} {p.phone}</li>
         })
     )
 }
